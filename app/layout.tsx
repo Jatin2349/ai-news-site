cfc08b70143e67a52666bccac7005fff1f525da2
Jatin2349 <kubar.jatin@gmail.com>
2025-10-19 16:14:19 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import Nav from '../components/Nav'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+
 
 export const metadata: Metadata = {
   title: {
