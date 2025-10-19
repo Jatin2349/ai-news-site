9cf22d2255163ba2d51dd09ebf1565c6cc8d6cb9
Jatin2349 <kubar.jatin@gmail.com>
2025-10-19 16:28:31 +0800

    Create site.ts

---
+++ lib/site.ts
@@ -0,0 +1,9 @@
+// lib/site.ts
+export const SOCIAL = {
+  x: '',                   // z.B. 'https://twitter.com/your_handle'
+  linkedin: '',            // z.B. 'https://www.linkedin.com/company/your_page'
+  instagram: '',           // z.B. 'https://www.instagram.com/your_handle'
+  threads: '',             // z.B. 'https://www.threads.net/@your_handle'
+  tiktok: '',              // z.B. 'https://www.tiktok.com/@your_handle'
+  email: ''                // z.B. 'hello@yourdomain.com'
+}
