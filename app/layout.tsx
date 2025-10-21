d71bc6d109530dc016c1b16219434e7a08946107
Jatin2349 <kubar.jatin@gmail.com>
2025-10-21 16:26:17 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 import Nav from '../components/Nav'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
-import SocialBar from './components/SocialBar'
+import SocialBar from '../components/SocialBar'
 
 
 export const metadata: Metadata = {
