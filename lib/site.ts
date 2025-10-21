5e52ef6567d6fae76ecfd8bda06de79e71aa52d5
Jatin2349 <kubar.jatin@gmail.com>
2025-10-21 16:59:03 +0800

    Update site.ts

--- lib/site.ts
+++ lib/site.ts
@@ -1,15 +1,11 @@
-// /lib/site.ts
+// lib/site.ts
 export const SOCIAL = {
-  // Twitter/X – beides anbieten, damit SocialBar.tsx beides versteht
-  x: '',                  // z.B. 'https://x.com/deinprofil'
-  twitter: '',            // optionaler Alias: 'https://twitter.com/deinprofil'
-
-  linkedin: '',           // 'https://www.linkedin.com/in/deinprofil'
-  instagram: '',          // ''
-  threads: '',            // 'https://www.threads.net/@deinprofil'
-  tiktok: '',             // 'https://www.tiktok.com/@deinprofil'
-
-  // Newsletter / E-Mail – ebenfalls mit Alias
-  newsletter: '',         // z.B. 'https://newsletter.deinname.com' ODER 'mailto:you@example.com'
-  email: ''               // optionaler Alias: 'mailto:you@example.com'
+  x: '',
+  twitter: '',
+  linkedin: '',
+  instagram: '',
+  threads: '',
+  tiktok: '',
+  newsletter: '', // z.B. dein Newsletter-Link ODER mailto
+  email: ''       // z.B. 'mailto:you@example.com'
 } as const
