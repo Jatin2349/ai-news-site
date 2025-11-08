6e2198bb5ab1d7ce15dfa354d855f8ea88b9fb15
Jatin2349 <kubar.jatin@gmail.com>
2025-11-09 02:19:04 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 // app/layout.tsx
 import type { Metadata } from "next";
-import "@/app/globals.css";
+import "./app/globals.css";
 import Nav from "../components/Nav";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
